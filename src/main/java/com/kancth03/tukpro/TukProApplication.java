d732350c8947e8d0f838846269527362ec90cddb
TaeHee00 <kancth03@gmail.com>
2023-10-15 03:07:33 +0900

    Springboot-init

---
+++ src/main/java/com/kancth03/tukpro/TukProApplication.java
@@ -0,0 +1,13 @@
+package com.kancth03.tukpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kancth03/tukpro/TukProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kancth03.tukpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
