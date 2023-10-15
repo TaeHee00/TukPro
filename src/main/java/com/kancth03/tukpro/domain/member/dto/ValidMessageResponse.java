7986b473da6dae40f643a03b3084946f7c565059
TaeHee00 <kancth03@gmail.com>
2023-10-15 20:16:45 +0900

    feat: valid 메세지 반환 DTO에 Signup valid 생성자추가

--- src/main/java/com/kancth03/tukpro/domain/member/dto/ValidMessageResponse.java
+++ src/main/java/com/kancth03/tukpro/domain/member/dto/ValidMessageResponse.java
@@ -3,7 +3,12 @@ package com.kancth03.tukpro.domain.member.dto;
 public record ValidMessageResponse(
         String message
 ) {
+    // 상속으로 묶어서 부모객체로 처리 가능한지 확인
     public static ValidMessageResponse ofLogin(LoginValid valid) {
+        return new ValidMessageResponse(valid.name());
+    }
+
+    public static ValidMessageResponse ofSignup(SignupValid valid) {
         return new ValidMessageResponse(valid.name());
     }
 }
