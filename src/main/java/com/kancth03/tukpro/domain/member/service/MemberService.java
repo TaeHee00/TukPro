28367e7fdecf4e8bb4dbb73319704cca93ec1820
SNOW <kancth03@gmail.com>
2023-10-15 13:16:44 +0900

    feat: 로그인 기능 추가를 위한 service, repository 추가

--- src/main/java/com/kancth03/tukpro/domain/member/service/MemberService.java
+++ src/main/java/com/kancth03/tukpro/domain/member/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.kancth03.tukpro.domain.member.service;
 
+import com.kancth03.tukpro.domain.member.dto.SigninMemberRequest;
+import com.kancth03.tukpro.domain.member.dto.SigninMemberResponse;
 import com.kancth03.tukpro.domain.member.dto.SignupMemberRequest;
 import com.kancth03.tukpro.domain.member.dto.SignupMemberResponse;
 import com.kancth03.tukpro.domain.member.entity.Member;
@@ -17,5 +19,12 @@ public class MemberService {
     public SignupMemberResponse signupMember(SignupMemberRequest request) {
         Member savedMember = memberRepository.save(request.toEntity());
         return SignupMemberResponse.entityToDto(savedMember);
+    }
+
+    public SigninMemberResponse signinMember(SigninMemberRequest request) {
+        Member member = memberRepository.findMemberByEmailAndPassword(request.email(), request.password())
+                .orElseThrow(() -> new IllegalArgumentException("[로그인 실패] Email: " + request.email()));
+
+        return SigninMemberResponse.entityToDto(member);
     }
 }
