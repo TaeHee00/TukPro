06d953eab6bf895395f05bededacb2c45ed97746
TaeHee00 <kancth03@gmail.com>
2023-10-15 18:34:11 +0900

    feat: 로그인 유효성 검사를 위한 내부 비즈니스 로직 구현

--- src/main/java/com/kancth03/tukpro/domain/member/service/MemberService.java
+++ src/main/java/com/kancth03/tukpro/domain/member/service/MemberService.java
@@ -1,13 +1,12 @@
 package com.kancth03.tukpro.domain.member.service;
 
-import com.kancth03.tukpro.domain.member.dto.SigninMemberRequest;
-import com.kancth03.tukpro.domain.member.dto.SigninMemberResponse;
-import com.kancth03.tukpro.domain.member.dto.SignupMemberRequest;
-import com.kancth03.tukpro.domain.member.dto.SignupMemberResponse;
+import com.kancth03.tukpro.domain.member.dto.*;
 import com.kancth03.tukpro.domain.member.entity.Member;
 import com.kancth03.tukpro.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -21,10 +20,24 @@ public class MemberService {
         return SignupMemberResponse.entityToDto(savedMember);
     }
 
-    public SigninMemberResponse signinMember(SigninMemberRequest request) {
+    public LoginMemberResponse loginMember(LoginMemberRequest request) {
         Member member = memberRepository.findMemberByEmailAndPassword(request.email(), request.password())
                 .orElseThrow(() -> new IllegalArgumentException("[로그인 실패] Email: " + request.email()));
 
-        return SigninMemberResponse.entityToDto(member);
+        return LoginMemberResponse.entityToDto(member);
+    }
+
+    public ValidMessageResponse loginValid(LoginMemberRequest request) {
+        LoginValid loginValid = LoginValid.EMAIL_MISMATCH;;
+        Optional<Member> findMember = memberRepository.findByEmail(request.email());
+
+        if (findMember.isPresent()) {
+            loginValid = LoginValid.PASSWORD_MISMATCH;
+        }
+        Optional<Member> loginValidMember = memberRepository.findMemberByEmailAndPassword(request.email(), request.password());
+        if (loginValidMember.isPresent()) {
+            loginValid = LoginValid.SUCCESS;
+        }
+        return ValidMessageResponse.ofLogin(loginValid);
     }
 }
